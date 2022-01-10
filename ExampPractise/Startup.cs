804249c920ffc9a861c6717c48117c4af68a0323
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-10 17:42:43 +0400

    :rocket:

--- ExampPractise/Startup.cs
+++ ExampPractise/Startup.cs
@@ -16,7 +16,9 @@ namespace ExampPractise
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -27,13 +29,11 @@ namespace ExampPractise
             }
 
             app.UseRouting();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
