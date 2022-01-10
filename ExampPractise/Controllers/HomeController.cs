3e93889a84e935183c56bd9ad75d63f554bdabbd
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-10 18:51:22 +0400

    18:51

--- ExampPractise/Controllers/HomeController.cs
+++ ExampPractise/Controllers/HomeController.cs
@@ -3,14 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExampPractise.DAL;
+using ExampPractise.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExampPractise.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public AppDbContext _context { get; }
+
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult>Index()
+        {
+
+            HomeVM homeVm = new HomeVM
+            {
+                FeatureCards = await _context.FeatureCards.ToListAsync()
+            };
+            return View(homeVm);
         }
     }
 }
---
+++ ExampPractise/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ExampPractise.Models;
+
+namespace ExampPractise.ViewModels
+{
+    public class HomeVM
+    {
+        public List<FeatureCard> FeatureCards { get; set; }
+    }
+}
