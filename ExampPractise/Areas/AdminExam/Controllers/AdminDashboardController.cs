e6a2ee569ffb4c89a60a4c77c8f74e8bca8effaf
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-10 20:02:37 +0400

    all

---
+++ ExampPractise/Areas/AdminExam/Controllers/AdminDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampPractise.Area.AdminExam.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        [Area("AdminExam")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
