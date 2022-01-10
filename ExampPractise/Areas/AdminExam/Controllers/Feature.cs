6634b5cfb940850ff53c89047e0381fdb76af708
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-10 19:58:57 +0400

    Last 7:58

---
+++ ExampPractise/Areas/AdminExam/Controllers/Feature.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExampPractise.Area.AdminExam.Controllers
+{
+    public class Feature : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
